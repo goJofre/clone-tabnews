28143991c0416319fc4b716cae771625ab130356
goJofre <115852252+goJofre@users.noreply.github.com>
2025-07-09 22:34:31 +0000

    fix: change join to resolve in api/v1/migrations

--- infra/database.js
+++ infra/database.js
@@ -3,7 +3,7 @@ import { Client } from "pg";
 async function query(queryObject) {
   let client;
   try {
-    client = await getNetClient();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
@@ -14,14 +14,14 @@ async function query(queryObject) {
   }
 }
 
-async function getNetClient() {
+async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "production" ? true : false,
+    ssl: getSSLValues(),
   });
 
   await client.connect();
@@ -30,7 +30,17 @@ async function getNetClient() {
 
 const database = {
   query,
-  getNetClient,
+  getNewClient,
 };
 
 export default database;
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "production" ? true : false;
+}
